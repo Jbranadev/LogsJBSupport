7dd87a6f77e7c767734746fcf4d5648dec0ab044
Jose Carlos Bran <jbranadev@gmail.com>
2024-09-29 09:19:16 -0600

    Preparación release 1.0.3

--- Readme.md
+++ Readme.md
@@ -199,14 +199,14 @@ Maven
 <dependency>
     <groupId>io.github.josecarlosbran</groupId>
     <artifactId>logsjbsupport</artifactId>
-    <version>1.0.2</version>
+    <version>1.0.3</version>
 </dependency>
 ~~~
 
 Gradle
 
 ~~~
-implementation 'io.github.josecarlosbran:logsjbsupport:1.0.2'
+implementation 'io.github.josecarlosbran:logsjbsupport:1.0.3'
 ~~~
 
 Para mayor información sobre como descargar LogsJBSupport desde otros
--- src/main/java/com/josebran/LogsJB/LogsJB.java
+++ src/main/java/com/josebran/LogsJB/LogsJB.java
@@ -178,6 +178,27 @@ public class LogsJB {
         }
     }
 
+    /**
+     * Retorna la propiedad que indica cada cuanto ciertos logs validar el tamaño del archivo log
+     * @return Cantidad de registros que deben escribirse para validar el tamaño del archivo log
+     */
+    public static Integer getValidarSize() {
+        return getInstance().validarSize;
+    }
+
+    /**
+     * Setea la Cantidad de registros que deben escribirse para validar el tamaño del archivo log
+     * @param validarSize Cantidad de registros que deben escribirse para validar el tamaño del archivo log
+     */
+    public static void setSizeLog(Integer validarSize) {
+        try {
+            getInstance().setValidarSize(validarSize);
+            System.setProperty(getInstance().LogsJBValidarSize, String.valueOf(validarSize));
+        } catch (Exception e) {
+            System.err.println("Excepcion capturada al tratar de setear la validación de tamaño del archivo " + validarSize + " Trace de la Exepción : " + ExceptionUtils.getStackTrace(e));
+        }
+    }
+
     /***
      * Obtiene el usuario del sistema sobre el cual corre la aplicación
      * @return Retorna un String con el nombre del usuario actual.
